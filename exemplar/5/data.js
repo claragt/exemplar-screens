2975a2c43bd0c0245f6443130c1a9679cc89d697
claragt <clara.teoh@digital.cabinet-office.gov.uk>
2015-01-06 18:37:12 +0000

    Added two journeys
    Added Get property information and Register views

--- exemplar/5/data.js
+++ exemplar/5/data.js
@@ -1,35 +1,63 @@
 var data = {
   "service": "5. Land registry",
-  "last-updated": "[DATE HERE]",
+  "last-updated": "6 January 2015",
   "userjourneys": [            
     {
-      "title": "[TITLE OF SCENARIO 1]",
+      "title": "Get property information",
       "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]",
+        "caption": "Start and search",
+        "imgref": "images/get-property-information/1.Find-property-information.png",
+      },{
+        "caption": "Select property",
+        "imgref": "images/get-property-information/2.Select-property.png",
+        "note": "Search results."
+      },{
+        "caption": "Property information",
+        "imgref": "images//get-property-information/3.5-Granary-Avenue-Dorchester-DT14YY.png"
         "note": [
-          "This is a note.",
-          "This is another note."
-        ]
+        	"A page for every registered property on GOV.UK.",
+        	"Anyone can view this without payment or verifying their identity."
+        	]
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]",
-        "note": "This is a note."
+        "caption": "Verify identity",
+        "imgref": "images/get-property-information/4.Verify-identity.png",
+        "note": "Uses GOV.UK verify."
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      }]
+        "caption": "Payment details",
+        "imgref": "images/get-property-information/5.Payment-details.png",
+        "note": "Payment may not be required."
+      },{
+        "caption": "Register view",
+        "imgref": "images/get-property-information/6.Official-register-of-title-DT122047.png",
+        "note": [
+        	"Other versions below.",
+        	"This is not an official copy of the register."
+        	]
+      }
+      
+      ]
     },{
-      "title": "[TITLE OF SCENARIO 2]",
+      "title": "Variations of the register vew",
       "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]"
+        "caption": "Alpha final version",
+        "imgref": "images/register-views/A.Official-register.of-title-DT122047.png"
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
+        "caption": "Earlier version",
+        "imgref": "images/register-views/B.Official-register-of-title-DT122047.png"
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      }]
+        "caption": "With collapsible bits",
+        "imgref": "images/register-views/C.Official-register-of-title-DT122047.png"
+      },{
+        "caption": "Using the existing ABC register structure",
+        "imgref": "images/register-views/D.Official-register-of-title-DT122047.png"
+      },{
+        "caption": "Readable register",
+        "imgref": "images/register-views/E.Land-Ownership-record-MARS0002.png",
+        "note": "Created on LR Hackday."
+      },{
+        "caption": "Changes to register",
+        "imgref": "images/register-views/Z.Changes-to-register-of-title-DT122047.png"
+      }
+      ]
     }
   ]}
